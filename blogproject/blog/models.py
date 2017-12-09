6ef13a97eb4a4fb06b749b42829b1c7bdbcdd5cf
mtian2017 <t_tamir@126.com>
2017-12-09 22:41:59 +0800

    learn up to step 7

--- blogproject/blog/models.py
+++ blogproject/blog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
+from django.utils.six import python_2_unicode_compatible
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
@@ -13,7 +15,9 @@ class Tag(models.Model):
     def __str__(self):
         return self.name
 
+@python_2_unicode_compatible
 class Post(models.Model):
+    ojects = models.Manager()
     title = models.CharField(max_length=70)
     body = models.TextField()
     created_time = models.DateTimeField()
@@ -25,5 +29,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-
+    
+    def get_absolute_url(self):
+        return reverse('blog:detail', kwargs={'pk':self.pk})
 # Create your models here.
--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Post
 
 def index(request):
-    return HttpResponse("欢迎访问我的博客首页!")
-# Create your views here.
+    post_list = Post.ojects.all().order_by('-created_time')
+    return render(request, 'blog/index.html',context={'post_list':post_list})
+
+def detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/detail.html', context={'post':post})
