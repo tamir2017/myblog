71c7c93f4a52c98f27b5921f93f16eaea09c01c1
mtian2017 <t_tamir@126.com>
2017-12-12 23:36:25 +0800

    comments

--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -2,6 +2,7 @@ import markdown
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Post, Category 
+from comments.forms import CommentForm
 
 def index(request):
     post_list = Post.ojects.all().order_by('-created_time')
@@ -15,7 +16,15 @@ def detail(request, pk):
                                      'markdown.extensions.codehilite',
                                      'markdown.extensions.toc',
                                   ])
-    return render(request, 'blog/detail.html', context={'post':post})
+    form = CommentForm()
+    comment_list = post.comment_set.all()
+
+    context = {'post':post,
+               'form':form,
+               'comment_list':comment_list
+              }
+
+    return render(request, 'blog/detail.html', context=context)
 
 def archives(request, year, month):
     post_list = Post.objects.filter(created_time__year=year,
