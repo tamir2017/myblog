a3e611f21fddcfa6b44589a308a2310914d00181
mtian2017 <t_tamir@126.com>
2017-12-20 17:49:16 +0800

    listview

--- blogproject/blog/models.py
+++ blogproject/blog/models.py
@@ -1,7 +1,10 @@
+import markdown
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 from django.utils.six import python_2_unicode_compatible
+from django.utils.html import strip_tags
+
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
@@ -41,4 +44,17 @@ class Post(models.Model):
     def increase_views(self):
     	self.views += 1
     	self.save(update_fields=['views'])
+
+    def save(self, *args, **kwargs):
+    	if not self.excerpt:
+    		md = markdown.Markdown(extensions = [
+    				'markdown.extensions.extra',
+    				'markdown.extensions.codehilite'
+    			])
+    		self.excerpt = strip_tags(md.convert(self.body))[:54]
+    		
+    	super(Post, self).save(*args, **kwargs)
+
+
+    	
 # Create your models here.
--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -3,39 +3,62 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Post, Category 
 from comments.forms import CommentForm
+from django.views.generic import ListView, DetailView
 
-def index(request):
-    post_list = Post.ojects.all().order_by('-created_time')
-    return render(request, 'blog/index.html',context={'post_list':post_list})
+class IndexView(ListView):
+	model = Post
+	template_name = 'blog/index.html'
+	context_object_name = 'post_list'
 
-def detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
+class PostDetailView(DetailView):
+	model = Post
+	template_name = 'blog/detail.html'
+	context_object_name = 'post'
 
-    post.increase_views()
-    
-    post.body = markdown.markdown(post.body,
+	def get(self, request, *args, **kwargs):
+		response = super(PostDetailView, self).get(request, *args, **kwargs)
+		self.object.increase_views()
+		return response
+
+	def get_object(self, queryset=None):
+		post = super(PostDetailView, self).get_object(queryset=None)
+		post.body = markdown.markdown(post.body,
                                   extensions=[
                                      'markdown.extensions.extra',
                                      'markdown.extensions.codehilite',
                                      'markdown.extensions.toc',
                                   ])
-    form = CommentForm()
-    comment_list = post.comment_set.all()
+		return post
 
-    context = {'post':post,
-               'form':form,
-               'comment_list':comment_list
-              }
+	def get_context_data(self, **kwargs):
+		context = super(PostDetailView, self).get_context_data(**kwargs)
+		form = CommentForm()
+		comment_list = self.object.comment_set.all()
+		context.update({
+			'form':form,
+			'comment_list':comment_list
+		})
+		return context
 
-    return render(request, 'blog/detail.html', context=context)
+class ArchivesView(ListView):
+	model = Post
+	template_name = 'blog/index.html'
+	context_object_name = 'post_list'
 
-def archives(request, year, month):
-    post_list = Post.objects.filter(created_time__year=year,
-                                    created_time__month=month
-                                    ).order_by('-created_time')
-    return render(request, 'blog/index.html', context={'post_list':post_list})
+	def get_queryset(self):
+		year = self.kwargs.get('year')
+		month = self.kwargs.get('month')
+		return super(ArchivesView, self).get_queryset().filter(created_time__year = year,created_time__month  = month)
 
-def category(request, pk):
-    cate = get_object_or_404(Category, pk=pk)
-    post_list = Post.objects.filter(category=cate).order_by('-created_time')
-    return render(request, 'blog/index.html', context={'post_list':post_list})
+class CategoryView(ListView):
+	model = Post
+	template_name = 'blog/index.html'
+	context_object_name = 'post_list'
+
+	def get_queryset(self):
+		cate = get_object_or_404(Category, pk=self.kwargs.get('pk'))
+		return super(CategoryView, self).get_queryset().filter(category=cate)
+		
+
+
+
