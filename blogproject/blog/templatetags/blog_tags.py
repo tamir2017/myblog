29cf362afddad6cba3f63ab969188cb009cf7a53
mtian2017 <t_tamir@126.com>
2017-12-21 15:11:13 +0800

    tag cloud

--- blogproject/blog/templatetags/blog_tags.py
+++ blogproject/blog/templatetags/blog_tags.py
@@ -1,4 +1,4 @@
-from ..models import Post, Category
+from ..models import Post, Category, Tag
 from django import template
 from django.db.models.aggregates import Count
 from blog.models import Category
@@ -17,4 +17,6 @@ def archives():
 def get_categories():
     return Category.objects.annotate(num_posts=Count('post')).filter(num_posts__gt=0)
 
-
+@register.simple_tag
+def get_tags():
+	return Tag.objects.annotate(num_posts=Count('post')).filter(num_posts__gt=0)
--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -1,7 +1,7 @@
 import markdown
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from .models import Post, Category 
+from .models import Post, Category, Tag
 from comments.forms import CommentForm
 from django.views.generic import ListView, DetailView
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
@@ -114,6 +114,15 @@ class CategoryView(ListView):
 	def get_queryset(self):
 		cate = get_object_or_404(Category, pk=self.kwargs.get('pk'))
 		return super(CategoryView, self).get_queryset().filter(category=cate)
+
+class TagView(ListView):
+	model = Post
+	template_name = 'blog/index.html'
+	context_object_name = 'post_list'
+
+	def get_queryset(self):
+		tag = get_object_or_404(Tag, pk=self.kwargs.get('pk'))
+		return super(TagView, self).get_queryset().filter(tags=tag)
 		
 
 
